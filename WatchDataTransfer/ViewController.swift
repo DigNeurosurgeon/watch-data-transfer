e034469570bdcba23b8d0e103918a29c2b51a01c
Pieter Kubben <pieter@kubben.nl>
2016-01-03 10:51:23 +0100

    Initial Commit

---
+++ WatchDataTransfer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchDataTransfer
+//
+//  Created by Pieter Kubben on 03-01-16.
+//  Copyright © 2016 DigitalNeurosurgeon.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
